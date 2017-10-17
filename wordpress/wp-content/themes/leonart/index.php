3d5ee50087b3e7cbc079b14e45dda7a5020fc1d0
stephecloutier <stephecloutier@gmail.com>
2017-10-17 09:20:51 +0200

    Started adding home content

--- wordpress/wp-content/themes/leonart/index.php
+++ wordpress/wp-content/themes/leonart/index.php
@@ -4,7 +4,22 @@
 */
 get_header();
 ?>
-<h1>Hello world!</h1>
+<main>
+    <div class="banner">
+        <span class="banner__title">Saint-Léon'Art</span>
+        <span class="banner__content"><?= get_field('banner-text'); ?></span>
+    </div>
+
+    <div class="intro">
+        <span class="intro__title">Un vaste parcours artistique</span>
+        <?= get_field('intro-text'); ?>
+        <a href="" class="intro__link arrow-link">En savoir plus <span class="hidden">sur l'évènement Saint-Léon'Art</span></a>
+    </div>
+
+    <section class="home-artists">
+        
+    </section>
+</main>
 
 
 <?php get_footer(); ?>
