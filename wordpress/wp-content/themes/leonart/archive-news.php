6175936081bfdc44e757257981d2fb6fca59a451
stephecloutier <stephecloutier@gmail.com>
2017-10-25 09:51:37 +0200

    Used function on archive news

--- wordpress/wp-content/themes/leonart/archive-news.php
+++ wordpress/wp-content/themes/leonart/archive-news.php
@@ -43,7 +43,7 @@ get_header();
                 <h3 class="news__title"><?= $fields['news-title']; ?></h3>
                 <time datetime=""><?= the_date('j F Y'); ?></time>
                 <?php $image = $fields['news-img'];?>
-                <img src="<?= $image['url']; ?>" width="<?= $image['sizes']['smallest-width']; ?>" height="<?= $image['sizes']['smallest-height']; ?>" alt="Photo de la news <?= $fields['news-title']; ?>">
+                <img src="<?= $image['url']; ?>" width="<?= $image['sizes']['smallest-width']; ?>" height="<?= $image['sizes']['smallest-height']; ?>" alt="<?php if(sl_get_image_alt('news-img')) echo sl_get_image_alt('news-img'); else echo 'Image de la news ' . $fields['news-title']; ?>">
                 <div class="news__content"><?= $fields['news-content']; ?></div>
                 <a href="<?= the_permalink(); ?>" class="news__link">En lire plus <span class="hidden">sur <?= $fields['news-title']; ?></span></a>
             </a>
