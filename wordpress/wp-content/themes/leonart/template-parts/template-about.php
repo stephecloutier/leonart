d8e806d2858e54c743e08a9e4bedaa958c71732d
stephecloutier <stephecloutier@gmail.com>
2017-11-08 22:40:41 +0100

    Completed (almost) about template

--- wordpress/wp-content/themes/leonart/template-parts/template-about.php
+++ wordpress/wp-content/themes/leonart/template-parts/template-about.php
@@ -40,11 +40,45 @@ $fields = get_fields();
             <?php endwhile; ?>
         </ul>
     </section>
-
+    <?php endif; ?>
     <div class="cta cta--white">
         Des questions sur Saint-Léon'Art&nbsp;?
         <a href="<?= sl_get_page_url('template-contact.php'); ?>" class="button--white" title="Aller sur la page contact">Contactez-nous&nbsp;!</a>
     </div>
+
+    <?php if($fields['about-numbers']): ?>
+    <section>
+        <h2 class="subtitle--underline">L'année <?= $fields['about-year']; ?> en chiffres</h2>
+        <ul class="numbers">
+            <li>
+                [image de l'icône]
+                <span class="number"><?= $fields['about-artists-nb']; ?></span>
+                artistes
+            </li>
+            <li>
+                [image de l'icône]
+                <span class="number"><?= $fields['about-visitors-nb']; ?></span>
+                visiteurs
+            </li>
+            <li>
+                [image de l'icône]
+                <span class="number"><?= $fields['about-expos-nb']; ?></span>
+                expositions
+            </li>
+        </ul>
+    </section>
+    <?php endif; ?>
+
+    <div class="cta cta--white">
+        Découvrez nos partenaires pour l'évènement&nbsp;!
+        <a href="<?= sl_get_page_url('template-partners.php'); ?>" class="button--white" title="Aller sur la page des partenaires">Voir les partenaires</a>
+    </div>
+
+    <?php if($fields['about-show-press']): ?>
+    <section id="press">
+        <h2 class="subtitle--simple">Espace presse</h2>
+        [post-type pour doc. de l'espace presse]
+    </section>
     <?php endif; ?>
 </main>
 
