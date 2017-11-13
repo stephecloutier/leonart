4441c6dfb4cc4c7734deab8ea19d60cfb1c63e3b
stephecloutier <stephecloutier@gmail.com>
2017-11-13 16:00:24 +0100

    Started style on news, changed html markup to add datetime attribute

--- wordpress/wp-content/themes/leonart/parts/news.php
+++ wordpress/wp-content/themes/leonart/parts/news.php
@@ -1,11 +1,27 @@
-<?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
-<?php $fields = get_fields(); $newsContent = sl_remove_all_tags($fields['news-content']); ?>
-<a href="<?= the_permalink(); ?>">
-    <h3 class="news__title"><?= $fields['news-title']; ?></h3>
-    <time datetime=""><?= the_date('j F Y'); ?></time>
-    <?php $image = $fields['news-img'];?>
-    <img src="<?= $image['sizes']['smallest']; ?>" alt="<?php if(sl_get_image_alt('news-img')) echo sl_get_image_alt('news-img'); else echo 'Image de la news ' . $fields['news-title']; ?>">
-    <p class="news__content"><?= sl_get_the_excerpt($newsContent, 280); ?></p>
-    <a href="<?= the_permalink(); ?>" class="home-news__link">En lire plus <span class="hidden">sur <?= $fields['news-title']; ?></span></a>
-</a>
-<?php endwhile; endif; ?>
+<div class="news__wrapper">
+    <?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
+    <?php
+        $fields = get_fields();
+        $newsContent = sl_remove_all_tags($fields['news-content']);
+        $date = new DateTime(get_the_date('d-m-Y'));
+        $datetime = strftime('%Y-%m-%dT%H:%M', $date->getTimestamp());
+
+    ?>
+    <div class="single-news">
+        <a href="<?= the_permalink(); ?>" class="single-news__inner">
+            <h3 class="single-news__title"><?= $fields['news-title']; ?></h3>
+            <time class="single-news__date" datetime="<?= $datetime; ?>">
+                <?= strftime('%d %B %Y', $date->getTimestamp()); ?>
+            </time>
+            <?php $image = $fields['news-img'];?>
+            <figure class="single-news__figure">
+                <img class="single-news__img" src="<?= $image['sizes']['smallest']; ?>" alt="<?php if(sl_get_image_alt('news-img')) echo sl_get_image_alt('news-img'); else echo 'Image de la news ' . $fields['news-title']; ?>">
+            </figure>
+            <p class="single-news__content"><?= sl_get_the_excerpt($newsContent, 280); ?></p>
+            <span href="<?= the_permalink(); ?>" class="single-news__link arrow-link arrow-link--raspberry">
+                En lire plus<span class="hidden"> sur <?= $fields['news-title']; ?></span><!--
+            --></span>
+        </a>
+    </div>
+    <?php endwhile; endif; ?>
+</div>
