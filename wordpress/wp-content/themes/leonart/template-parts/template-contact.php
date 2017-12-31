129eb849aa136a04091e4911505ad76e7892d218
stephecloutier <stephecloutier@gmail.com>
2017-12-31 16:53:08 +0100

    Positionned objects on contact page

--- wordpress/wp-content/themes/leonart/template-parts/template-contact.php
+++ wordpress/wp-content/themes/leonart/template-parts/template-contact.php
@@ -9,23 +9,33 @@ get_header();
 
 <main>
     <h1 class="main-title">Contact</h1>
+    <div class="contact__wrapper">
+        <div class="contact__inner">
+            <section class="contact__aside">
+                <h2 class="hidden">Informations sur l'ASBL organisatrice</h2>
+                <h3><?= $fields['contact-organizer-name']; ?></h3>
+                <div class="contact__infos">
+                    <span class="contact__infos--phone"><?= $fields['contact-organizer-phone']; ?></span>
+                    <div class="contact__infos--address">
+                        <?= $fields['contact-organizer-address']; ?>
+                    </div>
+                    <a href="mailto:<?= $fields['contact-organizer-mail']; ?>" class="contact__infos--mail" title="Envoyer un mail à <?= $fields['contact-organizer-name']; ?>"><?= $fields['contact-organizer-mail']; ?></a>
+                </div>
+            </section>
 
-    <section>
-        <h2 class="hidden">Informations sur l'ASBL organisatrice</h2>
-        <h3><?= $fields['contact-organizer-name']; ?></h3>
-        <div class="contact__infos">
-            <span class="contact__infos--phone"><?= $fields['contact-organizer-phone']; ?></span>
-            <div class="contact__infos--address">
-                <?= $fields['contact-organizer-address']; ?>
-            </div>
-            <a href="mailto:<?= $fields['contact-organizer-mail']; ?>" class="contact__infos--mail" title="Envoyer un mail à <?= $fields['contact-organizer-name']; ?>"><?= $fields['contact-organizer-mail']; ?></a>
+            <section class="contact__form">
+                <h2 class="hidden">Formulaire de contact</h2>
+                <?= do_shortcode('[contact-form-7 id="308" title="Formulaire de contact"]'); ?>
+            </section>
         </div>
-    </section>
+    </div>
 
-    <section>
-        <h2 class="hidden">Formulaire de contact</h2>
-        <?= do_shortcode('[contact-form-7 id="308" title="Formulaire de contact"]'); ?>
-    </section>
+    <div class="cta cta--raspberry contact__cta">
+        <div class="cta--inner">
+            <span class="cta__catch-phrase">Envie de voir notre programmation&nbsp;?</span>
+            <a href="<?= sl_get_page_url('template-program.php'); ?>" class="cta-archive cta-archive--white" title="Aller sur la page programme"><span class="cta-archive__text">Découvrez le programme</span></a>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
