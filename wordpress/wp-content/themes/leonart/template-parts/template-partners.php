fa04b314393a3a7003fb4ce91ecc2491b8ac4cee
stephecloutier <stephecloutier@gmail.com>
2017-12-31 16:12:31 +0100

    Added style on partners page, changed markup + added classes

--- wordpress/wp-content/themes/leonart/template-parts/template-partners.php
+++ wordpress/wp-content/themes/leonart/template-parts/template-partners.php
@@ -10,47 +10,71 @@ get_header();
 <main>
     <h1 class="main-title">Partenaires</h1>
     <p class="intro"><?= $fields['partners-intro']; ?></p>
-    <ul>
-        <?php
-            $posts = new WP_Query([
-                'showposts' => -1,
-                'post_type' => 'partners',
-                'orderby' => 'rand',
-            ]);
-        ?>
-        <?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
-            <?php $partners = get_fields(); ?>
-        <li>
-            <?php if($partners['partner-url']): ?>
-            <a href="<?= $partners['partner-url']; ?>">
-            <?php endif; ?>
+    <div class="partners__wrapper">
+        <ul class="partners">
+            <?php
+                $posts = new WP_Query([
+                    'showposts' => -1,
+                    'post_type' => 'partners',
+                    'orderby' => 'rand',
+                ]);
+            ?>
+            <?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
+                <?php $partners = get_fields(); ?>
+            <li class="partners__item">
+                <?php if($partners['partner-url']): ?>
+                <a class="partners__link" href="<?= $partners['partner-url']; ?>" title="Visiter le site web du partenaire <?= $partners['partner-name']; ?>">
+                <?php else: ?>
+                <div class="partners__border">
+                <?php endif; ?>
+                    <?php if($partners['partner-img']): ?>
+                    <img class="partners__img" src="<?= $partners['partner-img']['url']; ?>" alt="<?= $partners['partner-name']; ?>">
+                    <?php else: ?>
+                    <span class="partners__name"><?= $partners['partner-name']; ?></span>
+                    <?php endif; ?>
 
-            <?php if($partners['partner-img']): ?>
-            <img src="<?= $partners['partner-img']['url']; ?>" alt="<?= $partners['partner-name']; ?>">
-            <?php else: ?>
-            <span class="no-logo"><?= $partners['partner-name']; ?></span>
-            <?php endif; ?>
+                <?php if(!$partners['partner-url']): ?>
+                </div>
+                <?php else: ?>
+                </a>
+                <?php endif; ?>
 
-            <?php if($partners['partner-url']): ?>
-            </a>
-            <?php endif; ?>
-
-            <?php if($partners['partner-facebook']): ?>
-                <a href="<?= $partners['partner-facebook']; ?>" class="partner__social social-facebook" title="Aller sur le facebook de <?= $partners['partner-name']; ?>">Facebook</a>
-            <?php endif; ?>
-            <?php if($partners['partner-linkedin']): ?>
-                <a href="<?= $partners['partner-linkedin']; ?>" class="partner__social social-linkedin" title="Aller sur le linkedin de <?= $partners['partner-name']; ?>">LinkedIn</a>
-            <?php endif; ?>
-            <?php if($partners['partner-twitter']): ?>
-                <a href="<?= $partners['partner-twitter']; ?>" class="partner__social social-twitter" title="Aller sur le twitter de <?= $partners['partner-name']; ?>">Twitter</a>
-            <?php endif; ?>
-            <?php if($partners['partner-instagram']): ?>
-                <a href="<?= $partners['partner-instagram']; ?>" class="partner__social social-instagram" title="Aller sur le instagram de <?= $partners['partner-name']; ?>">Instagram</a>
-            <?php endif; ?>
-        </li>
-        <?php endwhile; endif; ?>
-    </ul>
-
+                <?php $socialIcons = socialIcons(); ?>
+                <?php
+                    if($partners['partner-facebook'] || $partners['partner-linkedin'] || $partners['partner-twitter'] || $partners['partner-instagram']):
+                ?>
+                <div class="partners__social-icons">
+                    <?php if($partners['partner-facebook']): ?>
+                        <a href="<?= $partners['partner-facebook']; ?>" class="partners__social l-social-nav__link l-social-nav__link--raspberry" title="Aller sur le facebook de <?= $partners['partner-name']; ?>">
+                            <?= $socialIcons['facebook']; ?>
+                            <span class="hidden">Facebook</span>
+                        </a>
+                    <?php endif; ?>
+                    <?php if($partners['partner-linkedin']): ?>
+                        <a href="<?= $partners['partner-linkedin']; ?>" class="partners__social l-social-nav__link l-social-nav__link--raspberry" title="Aller sur le linkedin de <?= $partners['partner-name']; ?>">
+                            <?= $socialIcons['linkedin']; ?>
+                            <span class="hidden">LinkedIn</span>
+                        </a>
+                    <?php endif; ?>
+                    <?php if($partners['partner-twitter']): ?>
+                        <a href="<?= $partners['partner-twitter']; ?>" class="partners__social l-social-nav__link l-social-nav__link--raspberry" title="Aller sur le twitter de <?= $partners['partner-name']; ?>">
+                            <?= $socialIcons['twitter']; ?>
+                            <span class="hidden">Twitter</span>
+                        </a>
+                    <?php endif; ?>
+                    <?php if($partners['partner-instagram']): ?>
+                        <a href="<?= $partners['partner-instagram']; ?>" class="partners__social  l-social-nav__link l-social-nav__link--raspberry" title="Aller sur le instagram de <?= $partners['partner-name']; ?>">
+                            <?= $socialIcons['instagram']; ?>
+                            <span class="hidden">Instagram</span>
+                        </a>
+                    <?php endif; ?>
+                </div>
+                <?php endif; ?>
+            </li>
+            <?php endwhile; endif; ?>
+        </ul>
+    </div>
+    
     <div class="cta">
         <div class="cta--inner">
             <span class="cta__catch-phrase">Qu'est-ce que Saint-Léon'Art&nbsp;?</span>
