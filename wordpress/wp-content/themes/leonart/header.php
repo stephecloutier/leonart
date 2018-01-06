b3e50a667c9598baa4b797fbc323527eaef3ed11
stephecloutier <stephecloutier@gmail.com>
2018-01-06 23:06:56 +0100

    Fixed problems on safari

--- wordpress/wp-content/themes/leonart/header.php
+++ wordpress/wp-content/themes/leonart/header.php
@@ -67,14 +67,14 @@
                     <?php
                         $socialFields = get_fields();
                         $socialIcons = socialIcons();
-                    ?>
-                    <li class="l-social-nav__item">
+                    ?><!--
+                    --><li class="l-social-nav__item">
                         <a class="l-social-nav__link l-social-nav__link--<?= $socialFields['social-name']; ?>" href="<?= $socialFields['social-link']; ?>" title="Aller sur le <?= $socialFields['social-name']; ?> de Saint-Léon'Art">
                             <?= $socialIcons[$socialFields['social-name']]; ?>
                             <span class="hidden"><?= $socialFields['social-name']; ?></span>
                         </a>
-                    </li>
-                <?php endwhile; endif; ?>
+                    </li><!--
+            --><?php endwhile; endif; ?>
             </ul>
         </nav>
 
